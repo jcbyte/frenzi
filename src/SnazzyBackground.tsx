1eae7d88e040d529d578ab6924fef11d266beaca
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-06-16 15:48:35 +0100

    refactored background to use tailwindcss

--- src/SnazzyBackground.tsx
+++ src/SnazzyBackground.tsx
@@ -3,9 +3,9 @@ import rightBackground from "./background/right.png";
 
 export default function SnazzyBackground() {
 	return (
-		<div style={{ position: "absolute", width: "100vw", height: "100vh", overflow: "hidden" }}>
-			<img src={leftBackground} className="absolute" style={{ left: -200, bottom: -200, width: 800, height: 800 }} />
-			<img src={rightBackground} className="absolute" style={{ right: -200, top: -200, width: 800, height: 800 }} />
+		<div className="absolute w-screen h-screen overflow-hidden">
+			<img src={leftBackground} className="absolute left-[-200px] bottom-[-200px] w-[800px] h-[800px]" />
+			<img src={rightBackground} className="absolute right-[-200px] top-[-200px] w-[800px] h-[800px]" />
 		</div>
 	);
 }
