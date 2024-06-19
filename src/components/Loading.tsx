a4c325c6b608e5dedd2359723aaa9f129500a994
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-06-19 19:16:35 +0100

    loading component now uses a state variable

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,11 @@
 import { Spinner } from "@nextui-org/react";
 
-export default function Loading() {
-	return (
+export default function Loading({ loaded, once: elem }: { loaded: boolean; once: JSX.Element }) {
+	const Loader = () => (
 		<div className="w-full flex justify-center p-10">
 			<Spinner label="Loading..." color="primary" size="lg" />
 		</div>
 	);
+
+	return loaded ? elem : <Loader />;
 }
