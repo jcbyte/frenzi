321d606ab1810f09130e675fa9924699696f62d3
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-06-22 12:25:36 +0100

    added basic layout of person page

--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -1,4 +1,9 @@
+import { Button } from "@nextui-org/react";
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
+import UnpaidCard from "../components/UnpaidCard";
+import { UserSettingsContext } from "../globalContexts";
+import { DEFAULT_PERSON_DATA } from "../static";
 import { PersonData } from "../types";
 
 export default function PersonPage({
@@ -10,10 +15,33 @@ export default function PersonPage({
 	peopleData: PersonData[];
 	setPeopleData: React.Dispatch<React.SetStateAction<PersonData[]>>;
 }) {
+	const { userSettings } = useContext(UserSettingsContext);
 	const { personIndex: personIndexStr } = useParams();
+
 	// (derived state)
 	let personIndex: number = Number(personIndexStr);
+	let personData: PersonData = !asSkeleton ? peopleData[personIndex] : DEFAULT_PERSON_DATA;
 
-	// TODO this page
-	return <div>PersonPage for {!asSkeleton ? peopleData[personIndex].name : "a skele is loading"}</div>;
+	return (
+		<>
+			<UnpaidCard
+				asSkeleton={asSkeleton}
+				balance={personData.distance * userSettings.costPerDistance}
+				distance={personData.distance}
+			/>
+			{/* // TODO change distances/balance panel */}
+			{/* // TODO edit button */}
+			<Button
+				color="danger"
+				variant="flat"
+				className="w-fit min-w-40"
+				onClick={() => {
+					// TODO delete
+					console.log("delete");
+				}}
+			>
+				Delete
+			</Button>
+		</>
+	);
 }
