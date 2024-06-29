7cf776e3fd81676ae1ebab71c8f934a4437b6d77
Joel Cutler <joelcutler108@gmail.com>
2024-06-29 11:43:25 +0100

    panels can now be changed from balance to distance

--- src/components/UserPanelModifier.tsx
+++ src/components/UserPanelModifier.tsx
@@ -1,13 +1,12 @@
 import { Select, SelectItem, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
 import { Key, useCallback, useContext } from "react";
 import { UserPanelsContext } from "../globalContexts";
-import { PanelConfig } from "../types";
+import { PanelConfig, PanelConfigType } from "../types";
 
 export default function UserPanelModifier({ asSkeleton }: { asSkeleton?: boolean }) {
 	const { userPanels, setUserPanels } = useContext(UserPanelsContext);
 
-	const renderCell = useCallback((panelConfig: PanelConfig, columnKey: Key) => {
-		console.log(panelConfig.type);
+	const renderCell = useCallback((panelConfig: PanelConfig, rowNum: number, columnKey: Key) => {
 		switch (columnKey) {
 			case "type":
 				return (
@@ -15,12 +14,14 @@ export default function UserPanelModifier({ asSkeleton }: { asSkeleton?: boolean
 						label="Balance/Distance"
 						popoverProps={{ className: "dark text-foreground" }}
 						selectedKeys={[panelConfig.type]}
-						// onChange={(newValue): any => {
-						// 	setUserPanels((prev) => {
-						// 		return prev;
-						//     // TODO change
-						// 	});
-						// }}
+						onChange={(newValue) => {
+							setUserPanels((prev) => {
+								let newUserPanels: PanelConfig[] = prev.map((panelConfig, i) => {
+									return i != rowNum ? panelConfig : { ...panelConfig, type: newValue.target.value as PanelConfigType };
+								});
+								return newUserPanels;
+							});
+						}}
 					>
 						{/* List out the valid distance units defined in `static.ts` */}
 						<SelectItem key={"currency"}>Balance</SelectItem>
@@ -42,7 +43,7 @@ export default function UserPanelModifier({ asSkeleton }: { asSkeleton?: boolean
 			<TableBody>
 				{userPanels.map((panelConfig: PanelConfig, i) => {
 					return (
-						<TableRow key={i}>{(columnKey) => <TableCell>{renderCell(panelConfig, columnKey)}</TableCell>}</TableRow>
+						<TableRow key={i}>{(columnKey) => <TableCell>{renderCell(panelConfig, i, columnKey)}</TableCell>}</TableRow>
 					);
 				})}
 			</TableBody>
