2e088d890ba231fc63bdb7346e5b1d62e95ac714
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-07-10 23:06:11 +0100

    increase version number

--- src/static.ts
+++ src/static.ts
@@ -1,6 +1,6 @@
 import { PanelConfig, PersonData, UserSettings, ValidCurrencies, ValidDistanceUnits } from "./types";
 
-export const VERSION = "v1.1";
+export const VERSION = "v1.2";
 
 // The default user settings when no user logged in or for new users
 export const DEFAULT_SETTINGS: UserSettings = {
