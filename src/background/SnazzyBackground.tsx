e054e1790ff1d86e3032a15b27766d57981cc601
Joel Cutler <joelcutler108@gmail.com>
2024-07-29 15:41:16 +0100

    updated background images to be fixed so they do not move when scrolling

--- src/background/SnazzyBackground.tsx
+++ src/background/SnazzyBackground.tsx
@@ -7,12 +7,12 @@ export default function SnazzyBackground() {
 		<div className="absolute w-screen h-screen overflow-hidden">
 			<img
 				src={leftBackground}
-				className="absolute left-[-200px] bottom-[-200px] w-[800px] h-[800px] select-none"
+				className="fixed left-[-200px] bottom-[-200px] w-[800px] h-[800px] select-none"
 				aria-label="Left background image"
 			/>
 			<img
 				src={rightBackground}
-				className="absolute right-[-200px] top-[-200px] w-[800px] h-[800px] select-none"
+				className="fixed right-[-200px] top-[-200px] w-[800px] h-[800px] select-none"
 				aria-label="Right background image"
 			/>
 		</div>
