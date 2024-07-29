fbb72198f2731f041fb726279159eb68aa90bc53
Joel Cutler <joelcutler108@gmail.com>
2024-07-29 15:40:28 +0100

    updated signature to be fixed so scrolling will not effect

--- src/components/Signature.tsx
+++ src/components/Signature.tsx
@@ -2,7 +2,7 @@ import { VERSION } from "../static";
 
 export default function Signature() {
 	return (
-		<div className="text-zinc-500 absolute right-2 bottom-2 text-right text-sm">
+		<div className="text-zinc-500 fixed right-2 bottom-2 text-right text-sm">
 			<p>Frenzi {VERSION}</p>
 			<p>By Joel Cutler</p>
 		</div>
