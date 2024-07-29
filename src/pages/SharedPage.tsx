0c02df99aef31c0a2e42328ad94bded9e32c6700
Joel Cutler <joelcutler108@gmail.com>
2024-07-29 12:36:10 +0100

    shared page now loads the shared data

--- src/pages/SharedPage.tsx
+++ src/pages/SharedPage.tsx
@@ -1,13 +1,47 @@
+import { Skeleton } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { useParams } from "react-router-dom";
+import UnpaidCard from "../components/UnpaidCard";
+import { getSharedPersonsData } from "../firestore/db";
+import { DEFAULT_PERSON_DATA } from "../static";
+import { PersonData } from "../types";
 
-// TODO this page
+// TODO skeleton here would be different, because loading separately
+// TODO get authors user settings
 
-export default function SharedPage() {
+export default function SharedPage({ asSkeleton = false }: { asSkeleton?: boolean }) {
 	const { author, personIndex: personIndexStr } = useParams();
+	const [personData, setPersonData] = useState<PersonData>(DEFAULT_PERSON_DATA);
+
+	useEffect(() => {
+		if (!author) {
+			toast.error(`Could not load user data: Undefined author`);
+		} else {
+			getSharedPersonsData(author, Number(personIndexStr))
+				.then((data) => {
+					console.log(data);
+					setPersonData(data);
+				})
+				.catch((err) => {
+					toast.error(`Could not load user data: ${err.message}`);
+				});
+		}
+	}, []);
 
 	return (
-		<div>
-			SharedPage from {author} for {personIndexStr}
-		</div>
+		<>
+			<Skeleton isLoaded={!asSkeleton} className="rounded-lg">
+				<p className="text-xl">{personData.name}</p>
+			</Skeleton>
+			<div className="flex gap-2 items-end">
+				<UnpaidCard
+					asSkeleton={asSkeleton}
+					// balance={personData.distance * userSettings.costPerDistance}
+					balance={0}
+					distance={personData.distance}
+				/>
+			</div>
+		</>
 	);
 }
