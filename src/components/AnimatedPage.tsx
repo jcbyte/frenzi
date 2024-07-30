fbb136fe71363451110fd9280014cf33477cd49d
Joel Cutler <joelcutler108@gmail.com>
2024-07-30 11:09:25 +0100

    added animation into animated page

--- src/components/AnimatedPage.tsx
+++ src/components/AnimatedPage.tsx
@@ -1,6 +1,16 @@
 import { motion } from "framer-motion";
 import { ReactNode } from "react";
 
+const animation = {
+	initial: { opacity: 0, x: 100 },
+	animate: { opacity: 1, x: 0 },
+	exit: { opacity: 0, x: -100 },
+};
+
 export default function AnimatedPage({ children }: { children: ReactNode }) {
-	return <motion.div>{children}</motion.div>;
+	return (
+		<motion.div variants={animation} initial="initial" animate="animate" exit="exit" transition={{ duration: 1 }}>
+			{children}
+		</motion.div>
+	);
 }
